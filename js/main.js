9185f2d4f084decfaafbadbcc2a5253d6e981a2a
AlysonSSantos <alysonsantos080@gmail.com>
2021-12-10 19:20:16 -0300

    estrutura dos arquivos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function submit(){
+    
+    var login=0; //true or false
+    var user=document.getElementsByName('emailUser')[0].value;
+    //user = user.toLowerCase(); -> se eu quisesse que não reconhecesse caracteres maiúsculos ou minúsculos
+    var password = document.getElementsByName("passUser")[0].value;
+    //password = password.toLowerCase();
+
+    if(user=="user007@gmail.com" && password=="A1b2c3"){
+        window.location = "main.html";
+        login = 1;
+    }if(login==0){
+        alert("Dados incorretos, por favor, tente novamente");
+    }
+}
