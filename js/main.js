6f28fc5004c26a6d14f97ef056f16952cf4a421c
AlysonSSantos <alysonsantos080@gmail.com>
2022-01-07 10:25:25 -0300

    loja/login

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ var itens = new Object();
 itens.tipo1 = "Camiseta";
 itens.tipo2 = "Casaco";
 itens.tipo3 = "Calça";
+itens.tipo4 = "Sapatênis"
 itens.marca1 = "Nike";
 itens.marca2 = "Adidas";
 itens.marca3 = "Topper";
+itens.marca4 = "Gucci";
 document.getElementById("produto001").innerHTML=itens.tipo1+" "+itens.marca3;
 document.getElementById("produto002").innerHTML=itens.tipo2+" "+itens.marca1;
-document.getElementById("produto003").innerHTML=itens.tipo3+" "+itens.marca2;
+document.getElementById("produto003").innerHTML=itens.tipo3+" "+itens.marca4;
+document.getElementById("produto004").innerHTML=itens.tipo4+" "+itens.marca2;
+document.getElementById("produto005").innerHTML=itens.tipo1+" "+itens.marca2;
+document.getElementById("produto006").innerHTML=itens.tipo4+" "+itens.marca4;
 }
 
 const login = () =>{
