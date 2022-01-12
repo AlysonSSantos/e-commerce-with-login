2d0b24cabc54dbd52fbad5ec8b61a49a00a8b0bd
AlysonSSantos <alysonsantos080@gmail.com>
2022-01-12 16:17:37 -0300

    loja virtual com login

--- js/comprando.js
+++ js/comprando.js
@@ -1,17 +1,16 @@
 const addFavoritos = () =>{
     document.getElementById("favoritos").style.color="blue"
     document.getElementById("favInfo").innerHTML="Adicionado aos favoritos!"
-    document.getElementById("removerFavorito").style.display="block"
     setTimeout(function(){
         document.getElementById("favInfo").innerHTML=""
-    }, 2500)
+    }, 1500)
 }
 
 const removerFavoritos = () => {
     document.getElementById("favoritos").style.color="black"
-    document.getElementById("favInfo").innerHTML="Removido dos favoritos"
+    document.getElementById("removeInfo").innerHTML="X Removido dos favoritos!"
     setTimeout(function(){
-        document.getElementById("favInfo").innerHTML=""
-    }, 2500)
+        document.getElementById("removeInfo").innerHTML="X"
+    }, 1500)
 }
 
