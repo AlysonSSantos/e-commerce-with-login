8746e8e4d0ae3835cd03e1b37d3096bfb6ec82a4
AlysonSSantos <alysonsantos080@gmail.com>
2022-01-13 23:06:05 -0300

    loja com login

--- js/comprando.js
+++ js/comprando.js
@@ -1,22 +1,22 @@
-const addFavoritos = () =>{
-    document.getElementById("favoritos").style.color="blue"
-}
+const addFavoritos = () => {
+    document.getElementById("favoritos").style.color = "blue";
+};
 
 const removerFavoritos = () => {
-    document.getElementById("favoritos").style.color="black"
-}
+    document.getElementById("favoritos").style.color = "black";
+};
 
 const confFrete = () => {
-    const frete = new Object ();
-    frete.categoria1="Frete grátis"
-    frete.categoria2="Envio: R$22,00"
-    frete.categoria3="Envio: R$10,82"
-    document.getElementById("freteInfo").innerHTML=frete.categoria1
-}
+    const frete = new Object();
+    frete.categoria1 = "Frete grátis";
+    frete.categoria2 = "Envio: R$22,00";
+    frete.categoria3 = "Envio: R$10,82";
+    document.getElementById("freteInfo").innerHTML = frete.categoria1;
+};
 
-const produtosEmEstoque = () =>{
-    var estoque = new Object()
-    estoque.numero="Em estoque: 10 Unidades"
-    estoque.unico="Modelo Único"
-    document.getElementById("produtosRestantes").innerHTML=estoque.numero;
-}
+const produtosEmEstoque = () => {
+    var estoque = new Object();
+    estoque.numero = "Em estoque: 10 Unidades";
+    estoque.unico = "Modelo Único";
+    document.getElementById("produtosRestantes").innerHTML = estoque.numero;
+};
